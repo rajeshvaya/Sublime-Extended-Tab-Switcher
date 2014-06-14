63d75191e71198ef846685d1c8ccd243abd7ad65
Rajesh Vaya <rajesh@vaya.local>
2014-06-14 13:32:51 +0400

    Combined active_group views only and other configurations for dirty file character from @drikin's OpenFileList repo

--- ExtendedSwitcher.py
+++ ExtendedSwitcher.py
@@ -1,6 +1,6 @@
 import sublime, sublime_plugin, os
 
-class ExtendedSwitcherCommand(sublime_plugin.TextCommand):
+class ExtendedSwitcherCommand(sublime_plugin.WindowCommand):
 	# declarations
 	open_files = []
 	open_views = []
@@ -8,20 +8,25 @@ class ExtendedSwitcherCommand(sublime_plugin.TextCommand):
 	settings = []
 
 	# lets go
-	def run(self, edit):
+	def run(self, list_mode):
 		# self.view.insert(edit, 0, "Hello, World!")
 		self.open_files = []
 		self.open_views = []
 		self.window = sublime.active_window()
 		self.settings = sublime.load_settings('ExtendedSwitcher.sublime-settings')
 		
-		for f in sublime.active_window().views():
+		for f in self.getViews(list_mode):
+			# if skip the current active is enabled do not add the current file it for selection
+			if self.settings.get('skip_current_file') == True:
+				if f.id() == self.window.active_view().id():
+					continue
+
 			self.open_views.append(f) # add the view object
 			file_name = f.file_name() # get the full path
 			
 			if file_name:
 				if f.is_dirty():
-					file_name += "*" # if there are any unsaved changes to the file
+					file_name += self.settings.get('mark_dirty_file_char') # if there are any unsaved changes to the file
 				self.open_files.append(os.path.basename(file_name))
 			else:
 				self.open_files.append("Untitled")
@@ -38,6 +43,7 @@ class ExtendedSwitcherCommand(sublime_plugin.TextCommand):
 
 		print selected
 
+	# sort the files for display in alphabetical order
 	def sort_files(self):
 		open_files = self.open_files
 		open_views = []
@@ -47,7 +53,7 @@ class ExtendedSwitcherCommand(sublime_plugin.TextCommand):
 		for f in open_files:
 			for fv in self.open_views:
 				if fv.file_name():
-					if (f == os.path.basename(fv.file_name())) or (f == os.path.basename(fv.file_name())+"*"):
+					if (f == os.path.basename(fv.file_name())) or (f == os.path.basename(fv.file_name())+self.settings.get('mark_dirty_file_char')):
 						open_views.append(fv)
 						self.open_views.remove(fv)
 
@@ -64,7 +70,18 @@ class ExtendedSwitcherCommand(sublime_plugin.TextCommand):
 		if self.settings.has("sort"):
 			return self.settings.get("sort", False)
 
-		
 
+	def getViews(self, list_mode):
+		views = []
+		# get only the open files for the active_group
+		if list_mode == "active_group":
+			views = self.window.views_in_group(self.window.active_group())
+
+		# get all open view if list_mode is window or active_group doesnt not have any files open
+		if (list_mode == "window") or (len(views) < 1):
+			views = self.window.views()
+
+		(_, current) = self.window.get_view_index(self.window.active_view())
+		return views
 
 
