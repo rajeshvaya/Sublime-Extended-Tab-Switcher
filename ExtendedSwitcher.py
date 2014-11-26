c722565990df4ddc5efb7a91e292741d3ddc48a2
rajeshvaya <vaya.rajesh@gmail.com>
2014-11-26 07:57:07 +0400

    show full path with relative path

--- ExtendedSwitcher.py
+++ ExtendedSwitcher.py
@@ -14,6 +14,7 @@ class ExtendedSwitcherCommand(sublime_plugin.WindowCommand):
 		self.open_views = []
 		self.window = sublime.active_window()
 		self.settings = sublime.load_settings('ExtendedSwitcher.sublime-settings')
+		folders = self.window.folders()
 		
 		for f in self.getViews(list_mode):
 			# if skip the current active is enabled do not add the current file it for selection
@@ -23,29 +24,35 @@ class ExtendedSwitcherCommand(sublime_plugin.WindowCommand):
 
 			self.open_views.append(f) # add the view object
 			file_name = f.file_name() # get the full path
-			
+			file_path = ''
+
 			if file_name:
+				for folder in folders:
+					if os.path.commonprefix([folder, file_name]) == folder:
+						file_path = os.path.relpath(file_name, folder)
+
 				if f.is_dirty():
 					file_name += self.settings.get('mark_dirty_file_char') # if there are any unsaved changes to the file
+
 				if self.settings.get('show_full_file_path') == True:
-					self.open_files.append(os.path.basename(file_name) + ' - ' + os.path.dirname(file_name)) 
+					self.open_files.append([os.path.basename(file_name), file_path]) 
 				else:
-					self.open_files.append(os.path.basename(file_name))
+					self.open_files.append([os.path.basename(file_name),]) 
 			elif f.name():
 				if f.is_dirty():
-					self.open_files.append(f.name() + self.settings.get('mark_dirty_file_char'))
+					self.open_files.append([f.name() + self.settings.get('mark_dirty_file_char'),])
 				else:
-					self.open_files.append(f.name())
+					self.open_files.append([f.name(),])
 			else:
 				if f.is_dirty():
-					self.open_files.append("Untitled"+self.settings.get('mark_dirty_file_char'))
+					self.open_files.append(["Untitled"+self.settings.get('mark_dirty_file_char'),])
 				else:
-					self.open_files.append("Untitled")
+					self.open_files.append(["Untitled",])
 
 		if self.check_for_sorting() == True:
 			self.sort_files()
  
-		self.window.show_quick_panel(self.open_files, self.tab_selected) # show the file list
+		self.window.show_quick_panel(self.open_files, self.tab_selected, False, -1) # show the file list
 
 	# display the selected open file
 	def tab_selected(self, selected):
@@ -62,6 +69,7 @@ class ExtendedSwitcherCommand(sublime_plugin.WindowCommand):
 		open_files.sort()
 
 		for f in open_files:
+			f = f[0]
 			for fv in self.open_views:
 				if fv.file_name():
 					f = f.replace(" - " + os.path.dirname(fv.file_name()),'')
